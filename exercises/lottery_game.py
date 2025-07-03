9e6b125f5bb6f64d48d0b1208b29c26961d81c9b
Geethanjali-Nagendra <geethanjali.nagendra@gmail.com>
2025-07-03 13:22:43 +0200

    Refactored the code as per review comments

--- exercises/lottery_game.py
+++ exercises/lottery_game.py
@@ -1,3 +1,5 @@
+import random 
+
 """Provided a list of lottery players, and also with 6 random lottery numbers.
 Find out the player with the most correct numbers, and print out their winnings and their name
 Task is to find who matched the most numbers, and print out a string with their name and the amount they won. For this exercise, 
@@ -6,7 +8,6 @@ For example: Jen won 1000.
 The winnings are calculated with this formula:
 winnings = 100 ** len(numbers_matched) """
 
-import random # built-in python module to generate random numbers
 lottery_numbers = set(random.sample(range(21), 6)) # generate 6 random numbers from 0 to 20
 players = [
     {"name": "Rolf", "numbers": {1, 3, 5, 7, 9, 11}},
@@ -18,9 +19,10 @@ max_correct = 0
 best_player = None
 for player in players:
     matched_numbers = player["numbers"].intersection(lottery_numbers)
-    
-    if len(matched_numbers) > max_correct:
-        max_correct = len(matched_numbers)
+    num_of_correct_matches = len(matched_numbers)
+
+    if num_of_correct_matches > max_correct:
+        max_correct = num_of_correct_matches
         best_player = player
 winnings = 100 ** max_correct
 print(f"{best_player['name']} won {winnings}.")
